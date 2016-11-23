7646420ee6404fd8c7844f6f492f43835deeb898
Matt Angus <mattangus02@gmail.com>
2016-11-22 21:48:02 -0500

    switched jdbc libs, and added database class

--- src/Database.java
+++ src/Database.java
@@ -1,4 +1,45 @@
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class Database {
+	private final static String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	private final static String DB_URL = "jdbc:mysql://gdansk/gamedata";
+	//  Database credentials
+	private final static String USER = "egta";
+	private final static String PASS = "gametheorypass";
 	
+	private ComboPooledDataSource ds;
+	
+	private static Database instance;
+	public static synchronized Database getInstance() throws PropertyVetoException
+	{
+		if(instance == null)
+			instance = new Database();
+		return instance;
+	}
+	
+	private Database() throws PropertyVetoException
+	{
+		ds = new ComboPooledDataSource();
+		ds.setDriverClass(JDBC_DRIVER);
+		ds.setJdbcUrl(DB_URL);
+		ds.setUser(USER);
+		ds.setPassword(PASS);
+	}
+	
+	public static synchronized void close()
+	{
+		if(instance != null)
+		{
+			instance.ds.close();
+		}
+	}
+	
+	public Connection getConnection() throws SQLException
+	{
+		return ds.getConnection();
+	}
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,40 +1,21 @@
+import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.ResultSet;
 
-import org.apache.commons.dbcp2.BasicDataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.sql.ResultSet;
 
 public class Driver {
 
-	public static void main(String[] args) throws SQLException {		
-		final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-		final String DB_URL = "jdbc:mysql://gdansk/gamedata";
-		
-		//  Database credentials
-		final String USER = "egta";
-		final String PASS = "gametheorypass";
-		
-		BasicDataSource ds = new BasicDataSource();
-		ds.setDriverClassName(JDBC_DRIVER);
-		ds.setUsername(USER);
-		ds.setPassword(PASS);
-		ds.setUrl(DB_URL);
-		
-		ds.setMinIdle(5);
-		ds.setMaxIdle(20);
-		ds.setMaxOpenPreparedStatements(180);
-		
-		Connection con = ds.getConnection();
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery("select * from profiles");
-		while (rs.next()) {
-			System.out.println("found a record");
+	public static void main(String[] args) throws SQLException, PropertyVetoException {
+		Connection[] cons = new Connection[5];
+		for(int i = 0; i < cons.length; i++)
+		{
+			cons[i] = Database.getInstance().getConnection();
 		}
-		System.out.println("done");
 		
-		if (rs != null) try { rs.close(); } catch (SQLException e) {e.printStackTrace();}
-        if (st != null) try { st.close(); } catch (SQLException e) {e.printStackTrace();}
-        if (con != null) try { con.close(); } catch (SQLException e) {e.printStackTrace();}
+		Database.close();
 	}
 }
